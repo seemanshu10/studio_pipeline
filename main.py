ee559af4ea429d451a958a6607e58becc7a9deb7
Seemanshu Verma <seemanshu0409@gmail.com>
2026-04-05 20:06:58 +0530

    added asset template added project folder creation

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import sys
 import json
 
 # current working dictionary 
-folder_path = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR  = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR  = os.path.join(BASE_DIR ,"folder_template")
 
-root_path = os.path.join(folder_path,"folder_template")
+# STUDIO_TEMPLATE  = os.path.join(TEMPLATE_DIR, "studio_template.json")
+# PROJECT_TEMPLATE  = os.path.join(TEMPLATE_DIR, "project_template.json")
 
-studio_template_path = os.path.join(root_path, "studio_template.json")
-project_template_path = os.path.join(root_path, "project_template.json")
-
-
-folder_pipeline_path= r"d:\PipelineTD\studio_pipeline\Pipeline"
+PIPELINE_ROOT = r"d:\PipelineTD\studio_pipeline\Pipeline"
 
 def load_json_data(filepath):
     """
-    load the json data 
+    load the json file and data safely
     
     Parameters : 
     filePath : First Number
@@ -26,44 +24,63 @@ def load_json_data(filepath):
     try:
         with open(filepath, "r") as json_file_data:
             content_json_data = json.load(json_file_data)
-
         return content_json_data    # returning json data 
     except FileNotFoundError:
         print(f"Error: File Path {filepath} not found! Check With TD.")
 
     except json.JSONDecodeError as e:
-        print(f"Error: Connot Read config File.")
+        print(f"Invalid JSON in {filepath}: {e}")
 
-def create_structure(studio_template_path, structure_dict):
-    for key, value in structure_dict.items():
-        folder_path = os.path.join(studio_template_path, key)
+def create_structure(folder_creation_path, structure_dict):
+    """
+    Recursively create folder structure 
+    
+    Parameters : 
+    studio_template_path : path for the folders 
+    structure_dict : dictionary date 
+
+    """
+    for nameFolder, subfolders in structure_dict.items():
+        folder_path = os.path.join(folder_creation_path, nameFolder)
         os.makedirs(folder_path, exist_ok=True)
         print(f"Created: {folder_path}")
 
-def build_studio(studio_template_path):
+def build_studio():
+    """
+    Setup studio pipeline structure.
+    
+    """
+    studio_template_path  = os.path.join(TEMPLATE_DIR, "studio_template.json")
     content_build_studio = load_json_data(studio_template_path)
-  
+    
     structure_dict = content_build_studio["studio_pipeline_folder"]
-    # print(structure_dict)
-    create_structure(folder_pipeline_path, structure_dict)
+    create_structure(PIPELINE_ROOT, structure_dict)
 
-def create_project(folder_path, project_name, project_template_path):
+def create_project(project_name):
+
+    """
+    Initialize project folders 
+    
+    """
+    project_template_path  = os.path.join(TEMPLATE_DIR, "project_template.json")
     project_template_folders = load_json_data(project_template_path)
+    # print(project_template_path)
     # print(project_template_folders)
-    # print(folder_path)
 
-    project_folder_path_root = os.path.join(folder_path, "projects")
-    project_folder_path = os.path.join(project_folder_path_root, project_name)
-
-    # print(project_folder_path)
+    project_root_path = os.path.join(PIPELINE_ROOT, "projects", project_name)
+    os.makedirs(project_root_path, exist_ok=True)
     structure_dict = project_template_folders["Project"]
-    # print(structure_dict)
 
-    create_structure(project_folder_path, structure_dict)
+    create_structure(project_root_path, structure_dict)
 
-# Run once (setup studio)
-build_studio(studio_template_path)
+def publish_versionup():
+    pass
 
-# Create new project
-create_project(folder_pipeline_path, "MyFilm", project_template_path)
-create_project(folder_pipeline_path, "MyFilm1", project_template_path)
+if __name__ == "__main__":
+
+    # Run once (setup studio)
+    build_studio()
+
+    # Create new project
+    create_project("MyFilm")
+    create_project("MyFilm1")
